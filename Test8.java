9bc9f209acdac090467bd687052fa09d04ef1943
Kooa_ <74341653+Kcsaw@users.noreply.github.com>
2022-10-07 11:40:48 +0800

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Test8 {
+    public static void main(String[] args) {
+        int grade = 0;
+        String space = " ";
+    while (grade < 10) {
+        System.out.printf("%d %n ", grade );
+        grade = grade + 1 ;
+        System.out.printf(space);
+        space = space +" ";
+        
+        
+     }
+    }
+}
