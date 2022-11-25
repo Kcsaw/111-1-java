cb264829e558549b47b11bc7dac1996ba2901ae3
Kooa_ <74341653+Kcsaw@users.noreply.github.com>
2022-11-25 11:10:41 +0800

    Add files via upload

---
+++ Test20.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.util.*;
+
+public class Test20 {
+    public static void main(String[] args) {
+    int K ;
+
+	Scanner input = new Scanner(System.in);
+	System.out.println("輸入公里數:");
+	K = input.nextInt();
+	
+	if (K > 7) {
+		System.out.println((85+ (K - 7) * 5)+"NT");
+	}
+	else {
+		System.out.println("85NT");
+	
+	}
+	}
+}
+	
